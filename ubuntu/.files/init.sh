87fd87b1e4b4c9fdd9221274684e1ad90a89341b
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-04-28 19:24:32 -0400

    Update init.sh

--- ubuntu/.files/init.sh
+++ ubuntu/.files/init.sh
@@ -1,6 +1,6 @@
 curl -sL https://raw.githubusercontent.com/mwilco03/BashBuddies/main/Scripts/.promptrc -o /home/${SIAB_USER}/.promptrc
 #cat /home/${SIAB_USER}/.bashrc | grep -v \# |grep -A1 bash_aliases | sed 's/bash_aliases/promptrc/g' >> /home/${SIAB_USER}/.bashrc 
-echo '[[ ! -f /home/${SIAB_USER}/.promptrc ]] || . /home/${SIAB_USER}/.promptrc' >> /home/${SIAB_USER}/.bashrc
+echo "[[ ! -f /home/${SIAB_USER}/.promptrc ]] || . /home/${SIAB_USER}/.promptrc" >> /home/${SIAB_USER}/.bashrc
 mkdir -p /home/${SIAB_USER}/.ssh
 curl -sL http://10.2.72.6:42069/.secret/win-95 -o /home/${SIAB_USER}/.ssh/win-95
 chown ${SIAB_USER} ${SIAB_GROUP} /home/${SIAB_USER}/.ssh/win-95
