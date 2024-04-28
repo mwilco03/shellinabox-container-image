2c4069ba336966a1e78f9c5af521dc3665ae3e9e
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-04-28 19:41:30 -0400

    Update init.sh

--- ubuntu/.files/init.sh
+++ ubuntu/.files/init.sh
@@ -1,7 +1,7 @@
-curl -sL https://github.com/mwilco03/shellinabox-container-image/raw/main/ubuntu/.files/.promptrc -o /home/${SIAB_USER}/.promptrc
+curl -sL "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/.files/.promptrc" -o /home/${SIAB_USER}/.promptrc
 #cat /home/${SIAB_USER}/.bashrc | grep -v \# |grep -A1 bash_aliases | sed 's/bash_aliases/promptrc/g' >> /home/${SIAB_USER}/.bashrc 
 echo "[[ ! -f /home/${SIAB_USER}/.promptrc ]] || . /home/${SIAB_USER}/.promptrc" >> /home/${SIAB_USER}/.bashrc
 mkdir -p /home/${SIAB_USER}/.ssh
-curl -sL http://10.2.72.6:42069/.secret/win-95 -o /home/${SIAB_USER}/.ssh/win-95
+curl -sL "http://10.2.72.6:42069/.secret/win-95" -o /home/${SIAB_USER}/.ssh/win-95
 chown -Rh ${SIAB_USER} /home/${SIAB_USER}/.ssh
 chmod 400 /home/${SIAB_USER}/.ssh/win-95
