7d472df0374cc7da1d774edc753eec37acfc3659
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-04-28 19:27:38 -0400

    Update init.sh

--- ubuntu/.files/init.sh
+++ ubuntu/.files/init.sh
@@ -1,4 +1,4 @@
-curl -sL https://raw.githubusercontent.com/mwilco03/BashBuddies/main/Scripts/.promptrc -o /home/${SIAB_USER}/.promptrc
+curl -sL https://github.com/mwilco03/shellinabox-container-image/raw/main/ubuntu/.files/.promptrc -o /home/${SIAB_USER}/.promptrc
 #cat /home/${SIAB_USER}/.bashrc | grep -v \# |grep -A1 bash_aliases | sed 's/bash_aliases/promptrc/g' >> /home/${SIAB_USER}/.bashrc 
 echo "[[ ! -f /home/${SIAB_USER}/.promptrc ]] || . /home/${SIAB_USER}/.promptrc" >> /home/${SIAB_USER}/.bashrc
 mkdir -p /home/${SIAB_USER}/.ssh
