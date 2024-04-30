898eafce84d1d328076fe8060a820eec2d5e66c0
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-04-30 14:36:33 -0400

    Update init.sh

--- ubuntu/.files/init.sh
+++ ubuntu/.files/init.sh
@@ -1,13 +1,13 @@
-curl -sL "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/.files/.promptrc" -o /home/${SIAB_USER}/.promptrc
+mkdir -p /home/${SIAB_USER}/.ssh /home/${SIAB_USER}/.student_files
+wget -P /home/${SIAB_USER}/ "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/.files/.promptrc" 
+for i in $(curl -sL "https://api.github.com/repos/mwilco03/shellinabox-container-image/contents/ubuntu/student_files?ref=main" | jq -r ".[].download_url");
+do wget -P /home/${SIAB_USER}/.student_files/ "$i"
+done
 #cat /home/${SIAB_USER}/.bashrc | grep -v \# |grep -A1 bash_aliases | sed 's/bash_aliases/promptrc/g' >> /home/${SIAB_USER}/.bashrc 
 echo "[[ ! -f /home/${SIAB_USER}/.promptrc ]] || . /home/${SIAB_USER}/.promptrc" >> /home/${SIAB_USER}/.bashrc
 echo "man_link() { echo "\"http://man.he.net/?topic=$1&section=all\"" ; } ; \n alias man='man_link' \n" >> ~/.bashrc
 echo -e "usat() { echo \"https://www.youtube.com/watch?v=ti9bzNWWTDY\" ;  } ; \n alias '--no-preserve-root /'='usat' \n" >> ~/.bashrc 
 echo -e "usat() { echo \"https://www.youtube.com/watch?v=ti9bzNWWTDY\" ;  } ; \n alias 'rm -rf'='usat' \n " >> ~/.bashrc 
-mkdir -p /home/${SIAB_USER}/.ssh /home/${SIAB_USER}/.student_files
 curl -sL "http://10.2.72.6:42069/.secret/win-95" -o /home/${SIAB_USER}/.ssh/win-95
-curl -sL "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/student_files/fruit.txt" -o /home/${SIAB_USER}/.student_files/fruit.txt
-curl -sL "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/student_files/two_cols.txt" -o /home/${SIAB_USER}/.student_files/two_cols.txt
-curl -sL "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/student_files/ps_example.txt" -o /home/${SIAB_USER}/.student_files/ps_example.txt
 chown -Rh ${SIAB_USER} /home/${SIAB_USER}/.ssh
 chmod 400 /home/${SIAB_USER}/.ssh/win-95
