7cb74a36a9de085880f753e95f11279f606c1a6b
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2024-05-01 13:33:05 -0400

    Update init.sh

--- ubuntu/.files/init.sh
+++ ubuntu/.files/init.sh
@@ -1,9 +1,12 @@
-mkdir -p "/home/${SIAB_USER}/.ssh" "/home/${SIAB_USER}/.student_files"
+mkdir -p "/home/${SIAB_USER}/.ssh" "/home/${SIAB_USER}/.student_files" "/home/${SIAB_USER}/scripts"
 wget -q -P "/usr/bin/" "https://raw.githubusercontent.com/mwilco03/GPSBL/master/browser_history.py"
 wget -q -P "/home/${SIAB_USER}/" "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/.files/.promptrc" 
 wget -q -P "/home/${SIAB_USER}/" "https://raw.githubusercontent.com/mwilco03/shellinabox-container-image/main/ubuntu/.files/.saferc" 
 for i in $(curl -sL "https://api.github.com/repos/mwilco03/shellinabox-container-image/contents/ubuntu/student_files?ref=main" | jq -r ".[].download_url");
 do wget -q -P "/home/${SIAB_USER}/.student_files/" "$i"
+done
+for i in $(curl -sL "https://api.github.com/repos/mwilco03/BashBuddies/contents/Scripts?ref=main" | jq -r ".[].download_url");
+do wget -q -P "/home/${SIAB_USER}/scripts/" "$i"
 done
 #cat /home/${SIAB_USER}/.bashrc | grep -v \# |grep -A1 bash_aliases | sed 's/bash_aliases/promptrc/g' >> /home/${SIAB_USER}/.bashrc 
 echo "[[ ! -f /home/${SIAB_USER}/.promptrc ]] || . /home/${SIAB_USER}/.promptrc" >> /home/${SIAB_USER}/.bashrc
